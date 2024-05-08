909f6f734150a4d40dbd1128cdcf6779b38bf31b
william <william.stucchi@mail.polimi.it>
2024-05-08 09:26:59 +0200

    genreralization of saving directory in training v2

--- scirob_submission/Model_Learning/models_v2/train_models_V2.py
+++ scirob_submission/Model_Learning/models_v2/train_models_V2.py
@@ -6,12 +6,13 @@ import numpy as np
 import math
 import datetime
 
-CALLBACKS = False
+CALLBACKS = True
 # create datetime-dependent paths
 path_day = datetime.datetime.now().strftime('%Y_%m_%d')
 path_datetime = datetime.datetime.now().strftime('%H_%M_%S')
+save_path_initial = '..saved_models/'
 
-mod = NN_model_V2()
+mod = NN_Model_V2()
 model_step_1 = mod.build_model(seed=1)
 model_step_2 = mod.build_model(seed=1)
 model_step_3 = mod.build_model(seed=1)
@@ -20,10 +21,10 @@ model_step_4 = mod.build_model(seed=1)
 # GET TRAINING DATA
 perc_validation = 0.2
 
-dataset_step_1 = np.loadtxt('../data/CRT/train_data_step1.csv', delimiter=',')
-dataset_step_2 = np.loadtxt('../data/CRT/train_data_step2.csv', delimiter=',')
-dataset_step_3 = np.loadtxt('../data/CRT/train_data_step3.csv', delimiter=',')
-dataset_step_4 = np.loadtxt('../data/CRT/train_data_step4.csv', delimiter=',')
+dataset_step_1 = np.loadtxt('../data/CRT/old/train_data_step1.csv', delimiter=',')
+dataset_step_2 = np.loadtxt('../data/CRT/old/train_data_step2.csv', delimiter=',')
+dataset_step_3 = np.loadtxt('../data/CRT/old/train_data_step3.csv', delimiter=',')
+dataset_step_4 = np.loadtxt('../data/CRT/old/train_data_step4.csv', delimiter=',')
 
 # 1 step
 train_features_step_1 = dataset_step_1[:math.floor(len(dataset_step_1) * (1 - perc_validation)), :-3]
@@ -49,7 +50,7 @@ val_features_step_4 = dataset_step_4[math.floor(len(dataset_step_4) * (1 - perc_
 train_labels_step_4 = dataset_step_4[:math.floor(len(dataset_step_4) * (1 - perc_validation)), -3:]
 val_labels_step_4 = dataset_step_4[math.floor(len(dataset_step_4) * (1 - perc_validation)):, -3:]
 
-for i in range(0, 2):
+for i in range(0, 1):
     # Step 1 training
     if CALLBACKS:
         es = tf.keras.callbacks.EarlyStopping(monitor='val_mse',
@@ -65,12 +66,12 @@ for i in range(0, 2):
                                                               min_delta=0.000005)
 
         mc = tf.keras.callbacks.ModelCheckpoint(
-            filepath='saved_models/step_1/callbacks/' + path_day + '/' + path_datetime + "/keras_model.h5",
+            filepath=save_path_initial +'/step_1/callbacks/' + path_day + '/' + path_datetime + "/keras_model.h5",
             monitor='val_mse',
             mode='min',
             verbose=1,
             save_best_only=True)
-        os.mkdir('results/step_1/callbacks/' + path_day + '/' + path_datetime + '/images')
+        # os.mkdir('results/step_1/callbacks/' + path_day + '/' + path_datetime + '/images')
 
         with tf.device('/GPU:0'):
             history_mod = model_step_1.fit(x=train_features_step_1,
@@ -84,19 +85,19 @@ for i in range(0, 2):
                                            use_multiprocessing=True)
     else:
         mc = tf.keras.callbacks.ModelCheckpoint(
-            filepath='saved_models/step_1/no_callbacks/' + path_day + '/' + path_datetime + "/keras_model.h5",
+            filepath=save_path_initial +'/step_1/no_callbacks/' + path_day + '/' + path_datetime + "/keras_model.h5",
             monitor='val_mse',
             mode='min',
             verbose=1,
             save_best_only=True)
-        os.mkdir('results/step_1/no_callbacks/' + path_day + '/' + path_datetime + '/images')
+        # os.mkdir('results/step_1/no_callbacks/' + path_day + '/' + path_datetime + '/images')
 
         with tf.device('/GPU:0'):
             history_mod = model_step_1.fit(x=train_features_step_1,
                                            y=train_labels_step_1,
                                            batch_size=1000,
                                            validation_data=(val_features_step_1, val_labels_step_1),
-                                           epochs=1500,
+                                           epochs=1000,
                                            verbose=1,
                                            shuffle=True,
                                            callbacks=[mc],
@@ -117,12 +118,12 @@ for i in range(0, 2):
                                                               min_delta=0.000005)
 
         mc = tf.keras.callbacks.ModelCheckpoint(
-            filepath='saved_models/step_2/callbacks/' + path_day + '/' + path_datetime + "/keras_model.h5",
+            filepath=save_path_initial +'/step_2/callbacks/' + path_day + '/' + path_datetime + "/keras_model.h5",
             monitor='val_mse',
             mode='min',
             verbose=1,
             save_best_only=True)
-        os.mkdir('results/step_2/callbacks/' + path_day + '/' + path_datetime + '/images')
+        # os.mkdir('results/step_2/callbacks/' + path_day + '/' + path_datetime + '/images')
 
         with tf.device('/GPU:0'):
             history_mod = model_step_2.fit(x=train_features_step_2,
@@ -136,19 +137,19 @@ for i in range(0, 2):
                                            use_multiprocessing=True)
     else:
         mc = tf.keras.callbacks.ModelCheckpoint(
-            filepath='saved_models/step_2/no_callbacks/' + path_day + '/' + path_datetime + "/keras_model.h5",
+            filepath=save_path_initial +'/step_2/no_callbacks/' + path_day + '/' + path_datetime + "/keras_model.h5",
             monitor='val_mse',
             mode='min',
             verbose=1,
             save_best_only=True)
-        os.mkdir('results/step_2/no_callbacks/' + path_day + '/' + path_datetime + '/images')
+        # os.mkdir('results/step_2/no_callbacks/' + path_day + '/' + path_datetime + '/images')
 
         with tf.device('/GPU:0'):
             history_mod = model_step_2.fit(x=train_features_step_2,
                                            y=train_labels_step_2,
                                            batch_size=1000,
                                            validation_data=(val_features_step_2, val_labels_step_2),
-                                           epochs=1500,
+                                           epochs=1000,
                                            verbose=1,
                                            shuffle=True,
                                            callbacks=[mc],
@@ -169,12 +170,12 @@ for i in range(0, 2):
                                                               min_delta=0.000005)
 
         mc = tf.keras.callbacks.ModelCheckpoint(
-            filepath='saved_models/step_3/callbacks/' + path_day + '/' + path_datetime + "/keras_model.h5",
+            filepath=save_path_initial +'/step_3/callbacks/' + path_day + '/' + path_datetime + "/keras_model.h5",
             monitor='val_mse',
             mode='min',
             verbose=1,
             save_best_only=True)
-        os.mkdir('results/step_3/callbacks/' + path_day + '/' + path_datetime + '/images')
+        # os.mkdir('results/step_3/callbacks/' + path_day + '/' + path_datetime + '/images')
 
         with tf.device('/GPU:0'):
             history_mod = model_step_3.fit(x=train_features_step_3,
@@ -188,19 +189,19 @@ for i in range(0, 2):
                                            use_multiprocessing=True)
     else:
         mc = tf.keras.callbacks.ModelCheckpoint(
-            filepath='saved_models/step_3/no_callbacks/' + path_day + '/' + path_datetime + "/keras_model.h5",
+            filepath=save_path_initial +'/step_3/no_callbacks/' + path_day + '/' + path_datetime + "/keras_model.h5",
             monitor='val_mse',
             mode='min',
             verbose=1,
             save_best_only=True)
-        os.mkdir('results/step_3/no_callbacks/' + path_day + '/' + path_datetime + '/images')
+        # os.mkdir('results/step_3/no_callbacks/' + path_day + '/' + path_datetime + '/images')
 
         with tf.device('/GPU:0'):
             history_mod = model_step_3.fit(x=train_features_step_3,
                                            y=train_labels_step_3,
                                            batch_size=1000,
                                            validation_data=(val_features_step_3, val_labels_step_3),
-                                           epochs=1500,
+                                           epochs=1000,
                                            verbose=1,
                                            shuffle=True,
                                            callbacks=[mc],
@@ -221,12 +222,12 @@ for i in range(0, 2):
                                                               min_delta=0.000005)
 
         mc = tf.keras.callbacks.ModelCheckpoint(
-            filepath='saved_models/step_4/callbacks/' + path_day + '/' + path_datetime + "/keras_model.h5",
+            filepath=save_path_initial +'/step_4/callbacks/' + path_day + '/' + path_datetime + "/keras_model.h5",
             monitor='val_mse',
             mode='min',
             verbose=1,
             save_best_only=True)
-        os.mkdir('results/step_4/callbacks/' + path_day + '/' + path_datetime + '/images')
+        # os.mkdir('results/step_4/callbacks/' + path_day + '/' + path_datetime + '/images')
 
         with tf.device('/GPU:0'):
             history_mod = model_step_4.fit(x=train_features_step_4,
@@ -240,22 +241,22 @@ for i in range(0, 2):
                                            use_multiprocessing=True)
     else:
         mc = tf.keras.callbacks.ModelCheckpoint(
-            filepath='saved_models/step_4/no_callbacks/' + path_day + '/' + path_datetime + "/keras_model.h5",
+            filepath=save_path_initial +'/step_4/no_callbacks/' + path_day + '/' + path_datetime + "/keras_model.h5",
             monitor='val_mse',
             mode='min',
             verbose=1,
             save_best_only=True)
-        os.mkdir('results/step_4/no_callbacks/' + path_day + '/' + path_datetime + '/images')
+        # os.mkdir('results/step_4/no_callbacks/' + path_day + '/' + path_datetime + '/images')
 
         with tf.device('/GPU:0'):
             history_mod = model_step_4.fit(x=train_features_step_1,
                                            y=train_labels_step_1,
                                            batch_size=1000,
                                            validation_data=(val_features_step_1, val_labels_step_1),
-                                           epochs=1500,
+                                           epochs=1000,
                                            verbose=1,
                                            shuffle=True,
                                            callbacks=[mc],
                                            use_multiprocessing=True)
 
-    CALLBACKS = True
+    CALLBACKS = False
