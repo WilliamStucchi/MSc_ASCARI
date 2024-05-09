cae3205a7db18d05855c60289f5712db6b5a77d5
william <william.stucchi@mail.polimi.it>
2024-05-09 16:03:27 +0200

    standalone tests script for NNVehicleDynamics

---
+++ NeuralNetwork_for_VehicleDynamicsModeling/run_tests_standalone.py
@@ -0,0 +1,98 @@
+import sys
+from random import random
+
+import numpy as np
+
+import src
+import helper_funcs_NN
+import visualization
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, mean_squared_error, mean_absolute_error
+
+
+np.random.seed(7)
+
+# ----------------------------------------------------------------------------------------------------------------------
+# Manage Paths ---------------------------------------------------------------------------------------------------------
+# ----------------------------------------------------------------------------------------------------------------------
+
+# create a dictionary which contains paths to all relevant folders and files
+path_dict = helper_funcs_NN.src.manage_paths.manage_paths()
+
+# ----------------------------------------------------------------------------------------------------------------------
+# Run Tests ------------------------------------------------------------------------------------------------------
+# ----------------------------------------------------------------------------------------------------------------------
+
+path_to_model = 'outputs/2024_05_08/15_23_02/keras_model_recurrent.h5'
+
+index_last_slash = path_to_model.rfind('/')
+path_to_results = path_to_model[:index_last_slash + 1]
+
+# create a dictionary which contains all parameters
+params_dict = helper_funcs_NN.src.handle_params.handle_params(path_dict=path_dict)
+
+for count in range(1, 3):
+
+    if params_dict['NeuralNetwork_Settings']['run_file_mode'] == 1:
+        print('STARTING RUN FEEDFORWARD NETWORK')
+
+        src.run_neuralnetwork.run_test_CRT(path_dict=path_dict,
+                                           params_dict=params_dict,
+                                           path_to_model=path_to_model,
+                                           nn_mode="feedforward",
+                                           counter=count)
+
+    if params_dict['NeuralNetwork_Settings']['run_file_mode'] == 2:
+        print('STARTING RUN RECURRENT NETWORK')
+
+        src.run_neuralnetwork.run_test_CRT(path_dict=path_dict,
+                                           params_dict=params_dict,
+                                           path_to_model=path_to_model,
+                                           nn_mode='recurrent',
+                                           counter=count)
+
+    # save and plot results (if activated in parameter file)
+    visualization.plot_results.plot_run_test_CRT(path_dict=path_dict,
+                                                 params_dict=params_dict,
+                                                 path_to_results=path_to_results,
+                                                 counter=count)
+
+print('---------------------------------------------------------------------------------------------------------------')
+
+# exit python if evaluation is disabled (NeuralNetwork_Settings.run_file_mode == 0)
+if params_dict['NeuralNetwork_Settings']['run_file_mode'] == 0:
+    sys.exit('SYSTEM EXIT: exit due to run_file_mode is set to zero to avoid testing the neural network against '
+             + 'vehicle sensor data')
+
+for i_count in range(0, params_dict['Test']['n_test']):
+
+    idx_start = params_dict['Test']['run_timestart'] + i_count * params_dict['Test']['iteration_step']
+
+    if params_dict['NeuralNetwork_Settings']['run_file_mode'] == 1:
+        print('STARTING RUN FEEDFORWARD NETWORK')
+
+        src.run_neuralnetwork.run_nn(path_dict=path_dict,
+                                     params_dict=params_dict,
+                                     path_to_model=path_to_model,
+                                     startpoint=idx_start,
+                                     counter=i_count,
+                                     nn_mode="feedforward")
+
+    if params_dict['NeuralNetwork_Settings']['run_file_mode'] == 2:
+        print('STARTING RUN RECURRENT NETWORK')
+
+        src.run_neuralnetwork.run_nn(path_dict=path_dict,
+                                     params_dict=params_dict,
+                                     path_to_model=path_to_model,
+                                     startpoint=idx_start,
+                                     counter=i_count,
+                                     nn_mode="recurrent")
+
+    # save and plot results (if activated in parameter file)
+    visualization.plot_results.plot_run(path_dict=path_dict,
+                                        params_dict=params_dict,
+                                        path_to_results=path_to_results,
+                                        counter=i_count,
+                                        start=idx_start)
+
+
+
