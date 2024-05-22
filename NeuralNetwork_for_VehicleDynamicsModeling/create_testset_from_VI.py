6ab594ce12935b4190d34bf60feb113121298815
william <william.stucchi@mail.polimi.it>
2024-05-22 15:16:47 +0200

    creating training and test set

--- NeuralNetwork_for_VehicleDynamicsModeling/create_testset_from_VI.py
+++ NeuralNetwork_for_VehicleDynamicsModeling/create_testset_from_VI.py
@@ -1,9 +1,12 @@
 import numpy as np
 import pandas as pd
+from tqdm import tqdm
 
 
 def create_test_set(path_to_test_set, path_to_input_set, number_of_tests):
-    for i in range(1, number_of_tests + 1, 1):
+    print('CREATING TEST DATA')
+
+    for i in tqdm(range(0, number_of_tests, 1)):
         # Load test set
         data = pd.read_csv(path_to_test_set + 'test_' + str(i) + '/DemoSportsCar_mxp.csv', dtype=object)
         data = data.drop(0, axis='rows')  # remove the row containing the measure units
@@ -48,15 +51,77 @@ def create_test_set(path_to_test_set, path_to_input_set, number_of_tests):
         dataframe = pd.DataFrame(test_set)
         dataframe.to_csv(path_to_input_set + '/test_set_'+str(i)+'.csv', index=False, header=False)
 
+    print('END CREATION TEST DATA')
+
+
 
 def create_training_set(path_to_training_set, path_to_input_set, number_of_sets):
-    for i in range(1,number_of_sets+1, 1):
-        # Load set
-        data = pd.read_csv(path_to_training_set + 'test_' + str(i) + '/DemoSportsCar_mxp.csv', dtype=object)
+    sum_ = 0
+    directories = ['mid_low_perf/', 'mid_perf/', 'mid_high_perf/', 'high_perf/']
+
+    print('CREATING TRAINING DATA')
+    for k, dir_ in enumerate(directories):
+        print('CREATING TRAINING DATA from ' + dir_)
+        for i in tqdm(range(0, number_of_sets, 1)):
+            # Load set
+            data = pd.read_csv(path_to_training_set + dir_ + 'test_' + str(i) + '/DemoSportsCar_mxp.csv', dtype=object)
+            data = data.drop(0, axis='rows')  # remove the row containing the measure units
+            data.reset_index(drop=True, inplace=True)
+
+            # print(data.head())
+
+            # Columns of interest
+            # Torques
+            trl = data['differential_torques.output_torque_left_rear'].to_numpy().astype(float)
+            trr = data['differential_torques.output_torque_right_rear'].to_numpy().astype(float)
+            tfl = data['differential_torques.output_torque_left_front'].to_numpy().astype(float)
+            tfr = data['differential_torques.output_torque_right_front'].to_numpy().astype(float)
+
+            tl = trl + tfl
+            tr = trr + tfr
+
+            # Brake pressures
+            bp_fl = data['Brake.Chamber_Pressure.L1'].to_numpy().astype(float)
+            bp_fr = data['Brake.Chamber_Pressure.R1'].to_numpy().astype(float)
+            bp_f = ((bp_fl + bp_fr) / 2) * 10  # average of the two lines and conversion from Mpa to bar
+
+            bp_rl = data['Brake.Chamber_Pressure.L2'].to_numpy().astype(float)
+            bp_rr = data['Brake.Chamber_Pressure.R2'].to_numpy().astype(float)
+            bp_r = ((bp_rl + bp_rr) / 2) * 10  # average of the two lines and conversion from Mpa to bar
+
+            # Steering
+            steer = data['driver_demands.steering'].to_numpy().astype(float)
+
+            # Accelerations
+            ax = data['Vehicle_States.longitudinal_acc_wrt_road'].to_numpy().astype(float)
+            ay = data['Vehicle_States.lateral_acc_wrt_road'].to_numpy().astype(float)
+            yaw_rate = data['Vehicle_States.yaw_angular_vel_wrt_road'].to_numpy().astype(float)
+
+            # Velocities
+            ux = data['Vehicle_States.longitudinal_vel_wrt_road'].to_numpy().astype(float)
+            uy = data['Vehicle_States.lateral_vel_wrt_road'].to_numpy().astype(float)
+
+            # Create test_set
+            set_ = np.transpose(np.array([ux, uy, yaw_rate, ax, ay, steer, tl, tr, bp_f, bp_r]))
+            # print(set_.shape)
+
+            # Save set
+            dataframe = pd.DataFrame(set_)
+            dataframe.to_csv(path_to_input_set + 'data_to_train_' + str(k * number_of_sets + i) + '.csv', index=False,
+                             header=False)
+
+            sum_ += len(set_)
+        print('END CREATIon TRAINING DATA from ' + dir_)
+
+    print('Total length: ' + str(sum_))
+
+
+def create_piste_training_complete(path_to_data, path_to_out, number_of_sets):
+    for i in tqdm(range(1, number_of_sets + 1, 1)):
+        # Load test set
+        data = pd.read_csv(path_to_data + 'test_' + str(i) + '/DemoSportsCar_mxp.csv', dtype=object)
         data = data.drop(0, axis='rows')  # remove the row containing the measure units
         data.reset_index(drop=True, inplace=True)
-
-        # print(data.head())
 
         # Columns of interest
         # Torques
@@ -90,20 +155,28 @@ def create_training_set(path_to_training_set, path_to_input_set, number_of_sets)
         uy = data['Vehicle_States.lateral_vel_wrt_road'].to_numpy().astype(float)
 
         # Create test_set
-        set_ = np.transpose(np.array([ux, uy, yaw_rate, ax, ay, steer, tl, tr, bp_f, bp_r]))
-        # print(set_.shape)
+        test_set = np.transpose(np.array([ux, uy, yaw_rate, ax, ay, steer, tl, tr, bp_f, bp_r]))
+        # print(test_set.shape)
 
-        # Save set
-        dataframe = pd.DataFrame(set_)
-        dataframe.to_csv(path_to_input_set+'data_to_train_'+str(i-1)+'.csv', index=False, header=False)
+        # Save test_set
+        dataframe = pd.DataFrame(test_set)
+        dataframe.to_csv(path_to_out + '/test_set_' + str(i) + '.csv', index=False, header=False)
+
+
+path_to_test_set = '../../CRT_data/flat/'
+
+"""path_to_input_set = 'inputs/trainingdata/piste_training_complete/'
+create_piste_training_complete(path_to_test_set, path_to_input_set, 32)"""
 
 
 # Create test set
 path_to_test_set = '../../CRT_data/test/'
-path_to_input_set = 'inputs/trainingdata/'
-create_test_set(path_to_test_set, path_to_input_set, 3)
+path_to_input_set = 'inputs/trainingdata/new/'
+create_test_set(path_to_test_set, path_to_input_set, 4)
 
 # Create training datasets
-path_to_training_set = '../../CRT_data/flat/'
-path_to_input_set = path_to_input_set + 'CRT/'
-create_training_set(path_to_training_set, path_to_input_set, 32)
+path_to_training_set = '../../CRT_data/'
+path_to_input_set = path_to_input_set
+create_training_set(path_to_training_set, path_to_input_set, 17)
+
+
